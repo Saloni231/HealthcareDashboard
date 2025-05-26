cb37c738a43fca3fa6f2ce38870a9699842f00ca
Saloni231 <ghagsaloni23@gmail.com>
2025-05-26 23:58:03 +0530

    Added Scanner and zoom in

--- src/components/atoms/Scanner/Scanner.jsx
+++ src/components/atoms/Scanner/Scanner.jsx
@@ -1,3 +1,4 @@
+import { ScanLine, ZoomIn } from "lucide-react";
 import classes from "./Scanner.module.css";
 
 const Scanner = () => {
@@ -5,6 +6,9 @@ const Scanner = () => {
     <>
       <div className={classes.heart}>❤️ Healthy Heart</div>
       <div className={classes.leg}>🦵🏻 Healthy Leg</div>
+      <ZoomIn size={20} color="#8b8f93" className={classes.zoomIn} />
+      <ScanLine size={60} color="#3534ad" className={classes.scannerHeart} />
+      <ScanLine size={60} color="#3534ad" className={classes.scannerLeg} />
     </>
   );
 };
